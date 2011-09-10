7d2eda7cc83d893d1175a46b07a59eee0e62090e
timotei <timotei_cluj@yahoo.co.uk>
2011-09-11 00:54:43 +0300

    Rename `Game1' to `TheStory'

---
+++ egp_story/Content.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace egp_story
+{
+	public class Assets
+	{
+		public static SpriteFont MainFont { get; private set; }
+
+		public static void LoadAssets( ContentManager content )
+		{
+			MainFont = content.Load<SpriteFont>( "Arial" );
+
+		}
+	}
+}
