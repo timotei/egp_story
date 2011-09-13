99d23c21c728f31eb6670dfea94dfba232548260
timotei <timotei_cluj@yahoo.co.uk>
2011-09-13 14:13:13 +0300

    Rewrite a bit the animated sprite

--- egp_story/AnimatedSprite.cs
+++ egp_story/AnimatedSprite.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace egp_story
@@ -8,35 +7,52 @@ namespace egp_story
 	{
 		private Texture2D _texture;
 		private Rectangle _sourceRectangle;
-		private int _count;
-		private int _speed;
+		private float _fps;
 		private int _increment;
-		private TimeSpan _currentGameTime;
+		private float _totalElapsed;
 
-		public AnimatedSprite( )
+		public Vector2 Position { get; set; }
+
+		public AnimatedSprite( Texture2D texture, int spriteCount, int speed )
 		{
-			_texture = Assets.SpriteAnim;
-			_count = 10;
-			_speed = 100;
+			_texture = texture;
+			_fps = ( float ) 1 / speed;
 
-			_increment = _texture.Width / _count;
+			_increment = _texture.Width / spriteCount;
 			_sourceRectangle = new Rectangle( 0, 0, _increment, _texture.Height );
 		}
 
 		public void Update( GameTime gameTime )
 		{
-			if ( ( gameTime.TotalGameTime - _currentGameTime ).Milliseconds > _speed ) {
+			if ( !Playing )
+				return;
+
+			_totalElapsed += ( float ) gameTime.ElapsedGameTime.TotalSeconds;
+			if ( _totalElapsed > _fps ) {
 
 				_sourceRectangle.X = ( _sourceRectangle.X + _increment ) % ( _texture.Width );
-				Console.WriteLine( _sourceRectangle.X );
 
-				_currentGameTime = gameTime.TotalGameTime;
+				_totalElapsed -= _fps;
 			}
 		}
 
 		public void Draw( SpriteBatch spriteBatch, GameTime gameTime )
 		{
-			spriteBatch.Draw( _texture, Vector2.Zero, _sourceRectangle, Color.White );
+			spriteBatch.Draw( _texture, Position, _sourceRectangle, Color.White );
+		}
+
+		/// <summary>
+		/// True if the sprite is being animated, false otherwise
+		/// </summary>
+		public bool Playing { get; set; }
+
+		/// <summary>
+		/// Resets the animation
+		/// </summary>
+		public void Reset( )
+		{
+			_totalElapsed = 0;
+			_sourceRectangle.X = 0;
 		}
 	}
 }
